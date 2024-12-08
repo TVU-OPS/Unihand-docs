feca2a2984c75711097c44aaeb4d4f2f3666a0e4
Tan Mai <tanmai833@gmail.com>
2024-12-08 16:12:31 +0700

    firts commit

---
+++ pages/_meta.ts
@@ -0,0 +1,22 @@
+import { title } from "process";
+
+export default {
+  index: {
+    description: "Welcome to the home page",
+  },
+  "getting-started": {
+    title: "🚀 Getting Started",
+    description: "Get started with the basics of using the app",
+  },
+  deploy: {
+    title: "🌐 Deploy to server",
+    description: "Learn how to deploy your app",
+  },
+  license: {
+    title: "📜 License",
+  },
+  contributing: {
+    title: "🤝 Contributing",
+    description: "Learn how to contribute to the project",
+  },
+};
