dcf5f582867124b2de483d28ae0eb5f4a77dc794
Tan Mai <tanmai833@gmail.com>
2024-12-08 22:40:02 +0700

    update: update readme

--- pages/deploy/_meta.ts
+++ pages/deploy/_meta.ts
@@ -1,10 +1,6 @@
 import { title } from "process";
 
 export default {
-  normal: {
-    title: "Triển khai hệ thống",
-    description: "Welcome to the home page",
-  },
   "use-docker": {
     title: "Sử dụng Docker Compose",
     description: "Learn how to deploy your app",
